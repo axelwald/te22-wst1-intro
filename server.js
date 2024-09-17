49b0effa3564e65f09ca852bf78c23d92bf519f7
Axel Waldenborg <149167584+axelwald@users.noreply.github.com>
2024-09-17 11:04:30 +0200

    aekfjaeif

--- server.js
+++ server.js
@@ -6,11 +6,14 @@ nj.configure("views", {
     autoescape: true,
     express: app,
 })
-
 app.use(express.static("public"))
+
+
+
 app.get("/", (request,response) => {
+    const name = request.query.name
     response.render("index.njk",{
-        message: "Hello world",
+        message: `Hello world ${name}`,
         title: "nj hello world",
     })
 })
@@ -23,7 +26,39 @@ app.get("/om",(request,response) => {
     )
 })
 
+const movies = {
+    "movie1": {
+        title: "movie1",
+        year: 2020,
+    }
+}
 
+app.get("/watch", (req,res) => {
+  
+    const movies = {
+        "movie1": {
+            title: "movie1",
+            year: 2020,
+        }
+    }
+    const movieId = req.query.v
+    const movie = movies[movieId]
+
+    res.render("watch.njk", {
+        movie: movie
+    })
+    // res.json(movie)
+})
+
+app.get("/ytub", (req,res) => {
+  
+    const id = req.query.v
+
+    res.render("ytub.njk", {
+        id: id
+    })
+    // res.json(movie)
+})
 app.listen(3000, () => {
     console.log("Server is running on https://localhost:3000")
 })
